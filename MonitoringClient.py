19bda09c24fe499236792d840cb617c959941d5d
qpwo5470 <qpwo5470@gmail.com>
2021-08-07 00:16:38 +0900

    time added

--- MonitoringClient.py
+++ MonitoringClient.py
@@ -1,32 +1,46 @@
 import threading
+import datetime
 import time
 import platform
 import requests
 import json
+import signal
 
 
 class MonitoringClient:
     def __init__(self, server_ip):
         self.server_ip = server_ip
-        self.data = {}
-        self.data['device_name'] = platform.node()
-        self.state = {}
-        self.state['app'] = 'Python3'
-        self.read_lock = threading.Lock()
-        self.thread = threading.Thread(target=self.send, args=())
-        self.thread.start()
+        self.__data = {}
+        self.__data['device_name'] = platform.node()
+        self.__state = {}
+        self.__state['app'] = 'Python3'
+        self.__read_lock = threading.Lock()
+        self.__state['start_time'] = datetime.datetime.now().strftime('%Y-%m-%d %A %H:%M:%S')
+        self.__thread = threading.Thread(target=self.__send, args=())
+        self.__thread.start()
+        signal.signal(signal.SIGTERM, self.__shutdown)
+        signal.signal(signal.SIGINT, self.__shutdown)
 
     def fakeDeviceName(self, name):
-        self.data['device_name'] = name
+        self.__data['device_name'] = name
 
     def set(self, name, value):
-        self.state[name] = value
+        self.__state[name] = value
 
-    def send(self):
+    def __send(self):
         while True:
-            self.data['data'] = json.dumps(self.state)
+            self.__data['data'] = json.dumps(self.__state)
             try:
-                requests.post(self.server_ip, data=self.data, timeout=60)
+                requests.post(self.server_ip, data=self.__data, timeout=60)
             except Exception as e:
                 print(e)
             time.sleep(5)
+
+    def __shutdown(self):
+        self.__state['exit_time'] = datetime.datetime.now().strftime('%Y-%m-%d %A %H:%M:%S')
+        self.__data['data'] = json.dumps(self.__state)
+        try:
+            requests.post(self.server_ip, data=self.__data, timeout=60)
+        except Exception as e:
+            print(e)
+
